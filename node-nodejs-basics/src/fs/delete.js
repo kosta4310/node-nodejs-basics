09a05bbb3b0e2e65522a81c8b2fdcfae49d4ca93
kosta4310 <realanka77@gmail.com>
2022-06-02 15:30:20 +0300

    feat: implement function fs delete

--- node-nodejs-basics/src/fs/delete.js
+++ node-nodejs-basics/src/fs/delete.js
@@ -1,3 +1,20 @@
+import { rejects } from 'assert';
+import fs from 'fs/promises';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const delPath = path.resolve(__dirname, './files/fileToRemove.txt');
+
 export const remove = async () => {
-    // Write your code here 
+    const del = fs.rm(delPath);
+    del.catch(err => {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        console.log(err);
+    })
 };
+remove();
