7abcbf24721c6f1cbfb7eb9c31932a5256e5f7fb
kosta4310 <realanka77@gmail.com>
2022-06-02 19:28:09 +0300

    feat: implement function fs read

--- node-nodejs-basics/src/fs/read.js
+++ node-nodejs-basics/src/fs/read.js
@@ -1,3 +1,29 @@
+import { rejects } from 'assert';
+import fs from 'fs/promises';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    // try {
+        
+                const reader = fs.readFile(path.resolve(__dirname, './files/fileToRead.txt'), {encoding: 'utf-8'});
+                reader.then(data => {console.log(data)})
+                .catch(error => {
+                    if (error.code === 'ENOENT') {
+                        throw new Error('FS operation failed');
+                    }
+                    console.log(error);
+                })
+       
+    // } catch (error) {
+    //     if (error.code === 'ENOENT') {
+    //         throw new Error('FS operation failed');
+    //     }
+    //     console.log(error);
+    // }
 };
+read();
