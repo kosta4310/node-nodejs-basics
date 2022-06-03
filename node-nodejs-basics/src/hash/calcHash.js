c8151197ef5290774f392a279f2212834817e55f
kosta4310 <realanka77@gmail.com>
2022-06-03 13:03:11 +0300

    feat: solve subtask hash

--- node-nodejs-basics/src/hash/calcHash.js
+++ node-nodejs-basics/src/hash/calcHash.js
@@ -1,3 +1,33 @@
+import { rejects } from 'assert';
+import fs from 'fs/promises';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import  crypto  from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const hash = crypto.createHash('sha256');
+
+
 export const calculateHash = async () => {
-    // Write your code here 
+   
+    try {
+        return await new Promise((resolve, reject) => {
+            resolve(
+                fs.readFile(path.resolve(__dirname, './files/fileToCalculateHashFor.txt'))
+                    .then(data => {
+                        return hash.update(data).digest('hex');
+                    
+                    
+                    }));
+            reject(new Error('new Error'));
+        })
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('No such file or directory')
+        }
+        console.log(error);
+    }
 };
