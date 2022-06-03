bbda816bd0ee823416e260a910f5aa063f8da77e
kosta4310 <realanka77@gmail.com>
2022-06-03 13:15:26 +0300

    feat: solve subtask streams read

--- node-nodejs-basics/src/streams/read.js
+++ node-nodejs-basics/src/streams/read.js
@@ -1,3 +1,15 @@
+
+import fs from 'fs';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream(path.resolve(__dirname, './files/fileToRead.txt'), { encoding: 'utf-8' });
+    rs.pipe(process.stdout);
 };
+read();
