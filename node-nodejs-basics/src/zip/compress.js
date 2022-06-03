a9e4d752383856052dd8f29b2053f04e4091526f
kosta4310 <realanka77@gmail.com>
2022-06-03 18:31:59 +0300

    feat: solve subtask zip

--- node-nodejs-basics/src/zip/compress.js
+++ node-nodejs-basics/src/zip/compress.js
@@ -1,3 +1,23 @@
+import fs from 'fs';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import  { createGzip }  from 'zlib';
+
+import { Transform } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const gzip = createGzip();
+const pathToReadFile = path.resolve(__dirname, './files/fileToCompress.txt');
+const pathToWriteFile = path.resolve(__dirname, './files/archive.gz');
+const rs = fs.createReadStream(pathToReadFile);
+const ws = fs.createWriteStream(pathToWriteFile);
+
 export const compress = async () => {
-    // Write your code here 
+    rs
+        .pipe(gzip)
+        .pipe(ws)
+    .on('error', (err) => {console.log(`I catch Error ${err}`)})
 };
+compress();
--- node-nodejs-basics/src/zip/decompress.js
+++ node-nodejs-basics/src/zip/decompress.js
@@ -1,3 +1,25 @@
+import fs from 'fs';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import  { createUnzip }  from 'zlib';
+
+import { Transform } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToReadFile = path.resolve(__dirname, './files/archive.gz');
+const pathToWriteFile = path.resolve(__dirname, './files/fileToCompress.txt');
+const dc = createUnzip();
+const rs = fs.createReadStream(pathToReadFile);
+const ws = fs.createWriteStream(pathToWriteFile);
+
+
 export const decompress = async () => {
-    // Write your code here 
+    rs
+        .pipe(dc)
+        .pipe(ws)
+    .on('error', (err) => {console.log(`I catch Error ${err}`)})
 };
+decompress();
