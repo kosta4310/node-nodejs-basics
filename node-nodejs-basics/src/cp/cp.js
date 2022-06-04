db4638e159682e3c6a335f9ad9a2f58675a820c7
kosta4310 <realanka77@gmail.com>
2022-06-04 11:08:37 +0300

    feat: solve child process

--- node-nodejs-basics/src/cp/cp.js
+++ node-nodejs-basics/src/cp/cp.js
@@ -1,3 +1,21 @@
+
+import fs from 'fs/promises';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { fork, spawn } from 'child_process';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const process = fork(path.resolve(__dirname, './files/script.js'), args);
+    // process.stdout.on('data', data => console.log(`stdout data: ${data}`));
+
+        
+    // process.send({ msg: 'welcome' });
+    // console.log(process.channel);
 };
+spawnChildProcess(['git','log']);
