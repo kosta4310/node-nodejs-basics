a60b26f4e0b820a23acebd52c9a1522b8c2c9526
kosta4310 <realanka77@gmail.com>
2022-06-05 21:10:09 +0300

    refactor: add coments

--- node-nodejs-basics/src/cli/index.js
+++ node-nodejs-basics/src/cli/index.js
@@ -1,6 +1,12 @@
 import { parseArgs } from "./args.js";
 import { parseEnv } from "./env.js";
 
+// Для проверки коментируйте один из вызовов функции
+
+/*Пример node src/cli --mode production --server false*/
 // parseArgs();
+
+/*for Windows write in powershell $env: RSS_any=9; node src/cli*/
 parseEnv();
-// for Windows write in powershell $env: RSS_any=9; node src/cli
+
+
