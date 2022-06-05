7ff93db68b0cd34e8e9c4730bc6c0d2de3ce489e
kosta4310 <realanka77@gmail.com>
2022-06-05 21:33:16 +0300

    rafactor: add coments

--- node-nodejs-basics/src/cp/cp.js
+++ node-nodejs-basics/src/cp/cp.js
@@ -12,15 +12,15 @@ const pathToFile = path.resolve(__dirname, './files/script.js');
 
 
 export const spawnChildProcess = async (args) => {
-    // const child_process = fork(path.resolve(__dirname, './files/script.js'), args);
-    
-    // child_process.on('exit', () => console.log('child: exit'));
-    // process.on('exit', () => console.log('master: exit'));
 
-    const child_process = spawn('node ' + pathToFile, args, {shell: true});
+    const child_process = fork(path.resolve(__dirname, './files/script.js'), args);
     
-    child_process.stdout.on('data', data => { process.stdout.write(data)});
-    process.stdin.on('data', (data) => child_process.stdin.write(data));
-    child_process.on('close', () => process.exit());
+    // Если вам не нравится скрипт с использованием fork, можете закоментировать
+    // строку выше и раскоментировать строки ниже с spawn
+    
+    // const child_process = spawn(`node ${pathToFile}`, args, {shell: true});
+    // child_process.stdout.on('data', data => { process.stdout.write(data)});
+    // process.stdin.on('data', (data) => child_process.stdin.write(data));
+    // child_process.on('close', () => process.exit());
 
 };
