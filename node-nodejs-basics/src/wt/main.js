b80322d12ccd038aab81633146b4bd9bf2a8c2ff
kosta4310 <realanka77@gmail.com>
2022-06-05 15:47:43 +0300

    feat: solve wt task

--- node-nodejs-basics/src/wt/main.js
+++ node-nodejs-basics/src/wt/main.js
@@ -1,3 +1,48 @@
+import fs from 'fs/promises';
+import path, { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { fork, spawn } from 'child_process';
+import { Worker } from 'worker_threads';
+import { stdout } from 'process';
+import  os  from 'os';
+import { rejects } from 'assert';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToFile = path.resolve(__dirname, './worker.js');
+const numberOfCore = os.cpus().length;
+let array = Promise.allSettled([]);
+
 export const performCalculations = async () => {
-    // Write your code here
+    
+    for (let i = 0; i < numberOfCore; i++) {
+        let n = 10 + i;
+        let promises = await new Promise((resolve, reject) => {
+            const worker = new Worker(pathToFile, {
+            
+            workerData: n
+        });
+        const workerId = worker.threadId;
+        console.log(`worker ${workerId} is started`);
+        // worker.on('message', (data) => { console.log(`data from worker: ${data}`) });
+        worker.on('message', (data) => {
+            // array.push({ [workerId]: data });
+            console.log(`workerId ${workerId}: ${data}`);
+            resolve(data);
+        });
+        // worker.on('exit', (code) => {
+        //     console.log(`worker ${workerId} exit ${code}`);
+        //     if (array.length === numberOfCore) {
+        //         console.log(array);
+        //     }
+        // });
+            worker.on('error', (err) => {
+                reject(err);
+            });
+        });
+        (await array).push(promises);
+    }
+    array.then(data => console.log(data));
+    
 };
--- node-nodejs-basics/src/wt/worker.js
+++ node-nodejs-basics/src/wt/worker.js
@@ -1,6 +1,24 @@
+import { resolve } from 'path';
+import { workerData, parentPort } from 'worker_threads';
+
+ 
 // n should be received from main thread
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+
 export const sendResult = () => {
+    let res = { status: 'error', data: null };
     // This function sends result of nthFibonacci computations to main thread
+    try {
+        let fib = nthFibonacci(workerData);
+        res.status = 'resolved';
+        res.data = fib;
+    } catch (error) {
+        throw error;
+    } finally {
+         parentPort.postMessage(`${JSON.stringify(res)}`);
+    }
+    
 };
+sendResult();
