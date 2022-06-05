4eb1a702dc4850cee0082ccea93224504dc6cb9c
kosta4310 <realanka77@gmail.com>
2022-06-05 22:37:06 +0300

    refactor: add coments

--- node-nodejs-basics/src/modules/index.js
+++ node-nodejs-basics/src/modules/index.js
@@ -4,3 +4,4 @@ import { createMyServer } from './cjsToEsm.mjs';
 console.log(unknownObject);
 
 createMyServer.listen(8000);
+// откройте браузер на вкладке localhost:8000 и вы увидете сообщение от сервера Request accepted
